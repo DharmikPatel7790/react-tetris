653c74fc4552f042140f398f37be75bbf3dedbb0
DharmikPatel7790 <40655174+DharmikPatel7790@users.noreply.github.com>
2026-06-05 12:15:52 +0530

    Update index.js

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import style from './index.less';
-import { transform, i18n, lan } from '../../unit/const';
-import { isMobile } from '../../unit';
-
 
 export default class Guide extends React.Component {
   constructor() {
     super();
-    this.state = {
-      isMobile: isMobile(),
-    };
+    this.state = {};
   }
   render() {
-    if (this.state.isMobile) {
-      return (
-        null
-      );
-    }
-    return (
-      <div style={{ display: 'none'}}>
-      
-      </div>
-    );
+    return ();
   }
 }
 
